133d5328498a1caed663b59e8cbfe467a0e85558
rahmatdenas <120230537+rahmatdenas@users.noreply.github.com>
2023-02-25 20:39:50 +0700

    Update eph-common.js

--- js/eph-common.js
+++ js/eph-common.js
@@ -10,8 +10,8 @@ const OSM_LAYER_ATTRIBUTION   = 'Base map &copy; <a href="https://www.openstreet
 const CARTO_LAYER_URL         = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png';
 const CARTO_LAYER_ATTRIBUTION = 'Base map &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a> (data), <a href="https://carto.com/">CARTO</a> (style)';
 const TILE_LAYER_MAX_ZOOM     = 19;
-const MIN_PH_LAT              =   3.5;
-const MAX_PH_LAT              =  1.2;
+const MIN_PH_LAT              =   1;
+const MAX_PH_LAT              =  4;
 const MIN_PH_LON              = 98.1;
 const MAX_PH_LON              = 102.1;
 
